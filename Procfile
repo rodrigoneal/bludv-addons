2ad07d5bf4fcbc7092ac87460ff23e43b3f222d9
Rodrigoneal <rodrigho2006@gmail.com>
2023-06-24 13:55:12 -0300

    mudando o procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn --host 0.0.0.0 --port $PORT api.main:app
+web: uvicorn --host 0.0.0.0 --port $PORT src.api.main:app
--- db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-
-def get_db(db_name: str):
-    client = MongoClient('localhost', 27017)
-    db = client["bludv"]
-    return db[db_name]
-
-
